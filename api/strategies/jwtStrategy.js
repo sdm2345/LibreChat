003d76af1e8fbbbe392d5f2f65381589f98e8e91
sdm <dongming.shen@sap.com>
2023-12-05 18:07:50 +0800

    Adding new features: 1:Automatically obtain the public key from the iss field in the JWT token for automatic validation. 2:For JWT validation mode: add a new Env(AUTO_CREATE_USER) switch to automatically create user information.

--- api/strategies/jwtStrategy.js
+++ api/strategies/jwtStrategy.js
@@ -1,21 +1,75 @@
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem'); // use jwk-to-pem
 
+const publicKeyCache = {};
+async function secretOrKeyProvider(request, rawJwtToken, done) {
+  // Parse the JWT header from the rawJwtToken.
+  const decoded = jwt.decode(rawJwtToken, { complete: true });
+  if (decoded && decoded.header) {
+    let alg = decoded.header.alg;
+    let iss = decoded.payload.iss;
+    let kid = decoded.payload.kid;
+    if (iss && kid) {
+      const cacheKey = `${iss}:${kid}`;
+      if (publicKeyCache[cacheKey]) {
+        console.error('hit publicKeyCache[cacheKey]', publicKeyCache[cacheKey]);
+        return done(null, publicKeyCache[cacheKey]);
+      }
+      const jwksUrl = `${iss}/.well-known/jwks.json`;
+      const jwksResponse = await fetch(jwksUrl);
+      const jwksData = await jwksResponse.json();
+      const publicKey = jwksData.keys.find((key) => key.kid === kid);
+      if (publicKey) {
+        // 缓存公钥
+        publicKeyCache[cacheKey] = jwkToPem(publicKey);
+      }
+    }
+    switch (alg) {
+      case 'HS256':
+        // 对于HS256，返回对称密钥
+        return done(null, process.env.JWT_SECRET);
+      default:
+        return done(new Error('Unsupported algorithm'), null);
+    }
+  } else {
+    return done(new Error('Invalid token'), null);
+  }
+}
+
+const { AUTO_CREATE_USER } = process.env ?? {};
 // JWT strategy
 const jwtLogin = async () =>
   new JwtStrategy(
     {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKeyProvider: secretOrKeyProvider,
+      algorithms: ['HS256', 'ES256'],
     },
     async (payload, done) => {
       try {
-        const user = await User.findById(payload.id);
+        const user =
+          (await User.findById(payload.id)) || (await User.findOne({ email: payload.email }));
         if (user) {
           done(null, user);
         } else {
-          console.log('JwtStrategy => no user found');
-          done(null, false);
+          if (AUTO_CREATE_USER) {
+            let email = payload.email || '';
+            let username = email.split('@')[0] || '';
+            let user = new User({
+              provider: 'jwt',
+              openidId: '',
+              username: username,
+              email: payload.email || '',
+              emailVerified: true,
+              name: username,
+            });
+            await user.save();
+            done(null, user);
+          } else {
+            done(null, false);
+          }
         }
       } catch (err) {
         done(err, false);
